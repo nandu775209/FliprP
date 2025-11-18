178ce9dc26bf59bbac6b3ea308ce1ceeb6ce47f8
Nandkishor mudafale <nkmudafale192@gamil.com>
2025-11-18 13:14:13 +0530

    chage01

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// Backend server ka base URL (aapka backend port)
-const BASE_URL = 'http://localhost:5000';
+
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
 
 const api = axios.create({
   baseURL: BASE_URL,
